785463b1ea44e35e9332194334f286334d8e29df
Michael Niedermayer <michael@niedermayer.cc>
2023-01-21 21:06:35 +0100

    mooltitestwalker: simpler and faster

--- mooltitestwalker.c
+++ mooltitestwalker.c
@@ -11,11 +11,11 @@ int main() {
                 break;
             v |= c<<6;
         }
-        if (i>2)
-            p += (v&195)%65 ? -1 : 3;
+        p += i>2 && (v^v>>6)&3;
+
         v>>=2;
     }
     printf("mooltiness: %8.2f expected: < 1 in 68%% cases, < 2 in 95%%, < 3 in 99.7%%\n",
-           fabs(p) / sqrt(3*i-9));
+           fabs(4*p-3*i+9) / sqrt(3*i-9));
     return 0;
 }
