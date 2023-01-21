a597ad2ae32a78095f2035250c2e035ead4bf08c
Michael Niedermayer <michael@niedermayer.cc>
2023-01-21 21:30:46 +0100

    mooltitestwalker: trivial simplification
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- mooltitestwalker.c
+++ mooltitestwalker.c
@@ -6,13 +6,13 @@ int main() {
     long long i,p;
     c=v=getchar();
     for(p=i=0; c!=EOF; i++) {
-        p += 1&&(v^v>>6)&3;
+        p += !((v^v>>6)&3);
 
         v>>=2;
         if (!(i&3))
             v |= (c = getchar())<<6;
     }
     printf("mooltiness: %8.2f expected: < 1 in 68%% cases, < 2 in 95%%, < 3 in 99.7%%\n",
-           fabs(4*p-3*i) / sqrt(3*i));
+           fabs(4*p-i) / sqrt(3*i));
     return 0;
 }
