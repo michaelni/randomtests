3a7b33f63feadb81a2b603530aaa83e8724057ff
Michael Niedermayer <michael@niedermayer.cc>
2024-01-08 17:24:25 +0100

    lcg: Allow selecting the base of the printed values
    Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

--- lcg.c
+++ lcg.c
@@ -8,7 +8,7 @@
 int main(int argc, char **argv)
 {
     if (argc < 6) {
-        fprintf(stderr, "lcg <seed bits> <multiplier> <increment> <seed> <count> [<output bits>]\n");
+        fprintf(stderr, "lcg <seed bits> <multiplier> <increment> <seed> <count> [<output bits> [<base>]]\n");
         fprintf(stderr, "When output bits is set to a value less than bits then the output will be a truncated LCG\n");
         return 1;
     }
@@ -18,6 +18,7 @@ int main(int argc, char **argv)
     uint64_t seed       = strtoull(argv[4], NULL, 0);
     uint64_t count      = strtoull(argv[5], NULL, 0);
     uint64_t output_bits= argc == 6 ? bits : strtoull(argv[6], NULL, 0);
+    uint64_t base       = argc  < 8 ? 10   : strtoull(argv[7], NULL, 0);
 
     if (bits > 64 || output_bits > bits) {
         fprintf(stderr, "bad bits\n");
@@ -41,7 +42,7 @@ int main(int argc, char **argv)
         seed *= multiplier;
         seed += increment;
         uint64_t out = (seed & mask) >> (bits - output_bits);
-        printf("%20"PRIu64" 0x%016"PRIX64"\n", out, out);
+        printf(base == 10 ? "%20"PRIu64"\n" : "0x%016"PRIX64"\n", out);
     }
     return 0;
 }
